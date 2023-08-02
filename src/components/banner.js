5f739e4720d30d73a02e580832d7f4dbe7eccf7f
Nandine <nandine0804@gmail.com>
2023-08-02 20:49:43 +0530

    completed 80% parts

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,21 +4,17 @@ const banner = () => {
   return (
     <div className="banner__pic">
       <h1>Ready to get started?</h1>
-      <hr/>
+      <hr />
       <div className="banner__con">
-        <p>
-          Fill this form below after filling the verification form above. You
-          shall get the verification code after filling the Google Form. New
-          Cohorts Start On 15th Of Every Month.
-        </p>
-      </div>
-      <div className="cir">
-        <a href="#about">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/4196/4196777.png"
-            alt="img"
-          />
-        </a>
+        <p>New Cohorts Start On 15th Of Every Month.Stay tuned!!</p>
+        <div className="cir">
+          <a href="#about">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/4196/4196777.png"
+              alt="img"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
